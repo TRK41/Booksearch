264d404aabc7f60afd61f541aa1d0c9e5267124f
TRK41 <Troyk@bell.net>
2023-01-23 01:46:28 -0500

    2nd commit

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -6,6 +6,8 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
+    bookCount:
+    savedBooks:
     thoughts: [Thought]!
   }
 
@@ -40,10 +42,9 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    saveBook(thoughtText: String!): Book
+    removeBook(book!, commentText: String!): Book
+    
   }
 `;
 
