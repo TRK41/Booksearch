ced61d4e915902b6035e4c9a39409a713f91ca45
TRK41 <Troyk@bell.net>
2023-02-07 02:09:19 -0500

    adding new apollo server with graphql

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -24,6 +24,17 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// Apollo server with GraphQL schema
+const startAppolloserver = async (typeDefs,resolvers) =>{
+await server.start();
+server.applyMiddleware({ app });
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  })
+})
+};
+
+startAppolloserver(typeDefs, resolvers);
