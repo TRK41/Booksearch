b69800a4f1788421efb8bea80ac090584eceeb70
TRK41 <Troyk@bell.net>
2023-02-10 23:18:39 -0500

    finishing up front end

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import { ADD_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -14,6 +18,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -56,7 +61,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    const saveBook = useMutation(ADD_BOOK);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -76,6 +81,8 @@ const SearchBooks = () => {
     } catch (err) {
       console.error(err);
     }
+    
+
   };
 
   return (
